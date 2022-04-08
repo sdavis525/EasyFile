21798c61a27f098768429ba9e006376eb9f2ffad
Sophia Davis <sophiadavis@Sophias-MBP.attlocal.net>
2022-04-08 12:43:57 -0400

    added licenceBadge and readme generates

--- assets/generateMarkdown.js
+++ assets/generateMarkdown.js
@@ -1,5 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  console.log(license)
+  let licenseType = license;
+  let altLicense = ''
+  
+  if(licenseType === 'MIT') {
+    "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  } else if (licenseType === 'Boost') {
+    "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+  } else if (licenseType === 'Unlicense') {
+    "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+  } else {
+    licenseType = altLicense
+  }
+  return licenseType;
+
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
